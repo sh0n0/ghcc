ee9219391daa712949ff19444864139177e1668f
kwkt <44708388+kwQt@users.noreply.github.com>
2020-07-02 22:16:31 +0900

    save scc result to a history file

--- ghcc.go
+++ ghcc.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/urfave/cli/v2"
 )
 
 const (
-	urlPrefix  = "https://"
-	hostPrefix = "github.com"
-	gitSuffix  = ".git"
+	urlPrefix       = "https://"
+	hostPrefix      = "github.com"
+	gitSuffix       = ".git"
+	historyFileName = ".ghcc_history"
 )
 
 func main() {
@@ -40,28 +45,17 @@ func getCodeAndCount(c *cli.Context) error {
 		return nil
 	}
 
-	filePath := c.Args().Get(0)
-	ghqArgs := []string{
-		"get",
-		filePath,
-	}
-	ghqOut, _ := exec.Command("ghq", ghqArgs...).CombinedOutput()
-	fmt.Println(string(ghqOut))
+	rowFilePath := c.Args().Get(0)
 
-	filePath = strings.TrimPrefix(filePath, urlPrefix)
-	filePath = strings.TrimSuffix(filePath, gitSuffix)
+	execGhqGet(rowFilePath)
 
-	if !strings.HasPrefix(filePath, hostPrefix) {
-		filePath = hostPrefix + "/" + filePath
-	}
+	filePath := trimFilePath(rowFilePath)
+	fullPath := getGhqRoot() + "/" + filePath
 
-	ghqRootArg := []string{"root"}
-	ghqRoot, _ := exec.Command("ghq", ghqRootArg...).Output()
-	rootPath := strings.TrimRight(string(ghqRoot), "\n")
+	showSccResult(fullPath)
 
-	fullPath := rootPath + "/" + filePath
-	out, _ := exec.Command("scc", fullPath).Output()
-	fmt.Println(string(out))
+	mainSummary := getSccSummaryForHistory(fullPath, filePath)
+	writeResultToHistory(mainSummary)
 
 	isTemporary := c.Bool("temporary")
 	if isTemporary {
@@ -92,4 +86,90 @@ interactive_loop:
 func ls(c *cli.Context) error {
 	// TODO
 	return nil
+}
+
+// Add new result to history
+// If a history file doesn't exist, create new one
+func writeResultToHistory(result string) {
+	homePath, _ := os.UserHomeDir()
+	historyPath := filepath.Join(homePath, historyFileName)
+	file, _ := os.OpenFile(historyPath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	defer file.Close()
+
+	fmt.Fprintln(file, result)
+}
+
+func showSccResult(fullPath string) {
+	sccArgs := []string{
+		"-s",
+		"lines",
+		fullPath,
+	}
+	sccOutput, _ := exec.Command("scc", sccArgs...).Output()
+	fmt.Println(string(sccOutput))
+}
+
+func trimFilePath(rowFilePath string) string {
+	rowFilePath = strings.TrimPrefix(rowFilePath, urlPrefix)
+	rowFilePath = strings.TrimSuffix(rowFilePath, gitSuffix)
+
+	if !strings.HasPrefix(rowFilePath, hostPrefix) {
+		rowFilePath = hostPrefix + "/" + rowFilePath
+	}
+	return rowFilePath
+}
+
+func execGhqGet(filePath string) {
+	ghqArgs := []string{
+		"get",
+		filePath,
+	}
+	fmt.Println("Fetching code...")
+	ghqOut, _ := exec.Command("ghq", ghqArgs...).CombinedOutput()
+	fmt.Println(string(ghqOut))
+}
+
+func getGhqRoot() string {
+	ghqRootArg := []string{"root"}
+	ghqRoot, _ := exec.Command("ghq", ghqRootArg...).Output()
+	rootPath := strings.TrimRight(string(ghqRoot), "\n")
+	return rootPath
+}
+
+func getSccSummaryForHistory(fullPath string, filePath string) string {
+	sccArgs := []string{
+		"-f",
+		"json",
+		"-s",
+		"lines",
+		fullPath,
+	}
+
+	sccOutputForHistory, _ := exec.Command("scc", sccArgs...).Output()
+
+	var summary []LanguageSummary
+	json.Unmarshal(sccOutputForHistory, &summary)
+	sortLaunguageSumamary(summary)
+	mainSummary := strings.TrimPrefix(filePath, hostPrefix) + " " + summary[0].Name + " " + strconv.FormatInt(summary[0].Lines, 10)
+	return mainSummary
+}
+
+// this will be removed when the next version of scc releases
+func sortLaunguageSumamary(summary []LanguageSummary) {
+	sort.Slice(summary, func(i, j int) bool {
+		return summary[i].Lines > summary[j].Lines
+	})
+}
+
+type LanguageSummary struct {
+	Name               string
+	Bytes              int64
+	Lines              int64
+	Code               int64
+	Comment            int64
+	Blank              int64
+	Complexity         int64
+	Count              int64
+	WeightedComplexity float64
+	Files              interface{}
 }
